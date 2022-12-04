5b7e80ff36dbbe277f2c9c55c1b2cea600fba982
bharr <bharr@DESKTOP-DCEIPGS>
2022-12-04 15:14:13 -0500

    Add project files.

---
+++ BankProject/Bank.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProject
+{
+    public class Bank
+    {
+        // Dictionary will store accounts
+        public Dictionary<string, Account> accounts = new Dictionary<string, Account>();
+        public string BankName;
+
+        // Assign bank name in constructor
+        public Bank(string BankingName)
+        {
+            BankName = BankingName;
+        }
+
+    }
+
+    // Base class for different account types
+    public class Account
+    {
+        public string OwnerName;
+        public int Balance;
+        public int WithdrawalLimit;
+
+        // Method to make deposits to account balance
+        public void Deposit(int Amount)
+        {
+            Balance += Amount;
+            Console.WriteLine("Money deposited to account, new balance is: " + Balance);
+
+            return;
+        }
+
+        // Method to make withdrawals from account balance
+        public void Withdraw(int Amount)
+        {
+
+            // Only withdraw if the account has enough money and is not over withdrawal limit
+            if (Balance >= Amount && (WithdrawalLimit >= Amount || WithdrawalLimit == 0))
+            {
+                Balance -= Amount;
+                Console.WriteLine("Money withdrawn from account, new balance is: " + Balance);
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds or over withdrawal limit...");
+            }
+
+            return;
+        }
+
+        // Method to make transfers from a target account to a host account
+        public void Transfer(Account Target, int Amount)
+        {
+
+            // Only withdraw if the target account has enough money
+            if (Target.Balance >= Amount)
+            {
+                Target.Balance -= Amount;
+                Console.WriteLine("Money withdrawn from target account, new balance is: " + Target.Balance);
+                Balance += Amount;
+                Console.WriteLine("Money deposited to account, new balance is: " + Balance);
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds on target account...");
+            }
+
+            return;
+        }
+    }
+
+    // Base Checking account
+    public class Checking : Account
+    {
+        public Checking(string Owner, int StartingBalance)
+        {
+            OwnerName = Owner;
+            Balance = StartingBalance;
+        }
+    }
+
+    // Base Investment account
+    public class Investment : Account
+    {
+
+    }
+
+    // Individual account with withdrawal limit of 500
+    public class Individual : Investment
+    {
+
+        public Individual(string Owner, int StartingBalance)
+        {
+            OwnerName = Owner;
+            Balance = StartingBalance;
+            WithdrawalLimit = 500;
+        }
+    }
+
+    // Corporate account
+    public class Corporate : Investment
+    {
+        public Corporate(string Owner, int StartingBalance)
+        {
+            OwnerName = Owner;
+            Balance = StartingBalance;
+        }
+    }
+}
---
+++ BankProject/UnitTesting.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProject
+{
+    internal class UnitTesting
+    {
+
+        public Bank BigBank = new Bank("WellsFargo");
+
+        private void InitializeBank()
+        {
+            // Fill bank with 3 types of accounts and starting balances
+            Checking paul = new Checking("Paul", 0);
+            Individual anne = new Individual("Anne", 2000);
+            Corporate kar = new Corporate("KarGlobal", 1000000);
+
+            BigBank.accounts.Add(paul.OwnerName, paul);
+            BigBank.accounts.Add(anne.OwnerName, anne);
+            BigBank.accounts.Add(kar.OwnerName, kar);
+
+            return;
+        }
+
+        private void TestWithdrawals()
+        {
+
+            // Test withdrawing with not enough funds edge case, expecting false
+            BigBank.accounts["Paul"].Withdraw(400);
+
+            // Test withdrawing with enough funds at withdrawal limit for individual, expecting true
+            BigBank.accounts["Anne"].Withdraw(500);
+
+            // Test withdrawing while over the withdrawal limit for individual account with enough money, expecting false
+            BigBank.accounts["Anne"].Withdraw(510);
+
+            // Test withdrawing past withdrawal limit for corporate account, expecting true
+            BigBank.accounts["KarGlobal"].Withdraw(50000);
+
+            return;
+        }
+
+        private void TestDeposits()
+        {
+
+            // Test depositing money with all accounts
+            BigBank.accounts["Paul"].Deposit(1);
+            BigBank.accounts["Anne"].Deposit(1000);
+            BigBank.accounts["KarGlobal"].Deposit(100000);
+
+            return;
+        }
+
+        private void TestTransfers()
+        {
+            // Test transfering with not enough funds on target edge case, expecting false
+            BigBank.accounts["Anne"].Transfer(BigBank.accounts["Paul"], 400);
+
+            // Test transfering with enough funds, expecting true
+            BigBank.accounts["Paul"].Transfer(BigBank.accounts["Anne"], 500);
+
+            // Test withdrawing with enough funds corporate, expecting true
+            BigBank.accounts["KarGlobal"].Transfer(BigBank.accounts["Anne"], 50);
+
+            return;
+
+        }
+
+        private static void Main(string[] args)
+        {
+
+            UnitTesting test = new UnitTesting();
+
+            // Display bank name and run tests
+            Console.WriteLine("Bank Name: " + test.BigBank.BankName);
+            test.InitializeBank();
+            test.TestWithdrawals();
+            test.TestDeposits();
+            test.TestTransfers();
+
+            return;
+        }
+
+    }
+
+}
